6f0c3d00bf018dd77f3d4c72ff24a5eeac5d88a1
Olivia Kesler <oakesler@protonmail.ch>
2019-09-11 04:25:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
     @c = c
   end
   
-  def kind
+  def self.kind
     if (a == 0 || b == 0 || c == 0) || ((a + b) < c || (b + c) < a || (a + c) < b)
       begin
       raise TriangleError
